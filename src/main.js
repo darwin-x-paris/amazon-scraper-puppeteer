9cb15a964505fb03207d4d1809867b712649e0c3
name <name@email.com>
2022-09-20 09:57:56 +0200

    Use incognito

--- src/main.js
+++ src/main.js
@@ -36,6 +36,9 @@ Apify.main(async () => {
 
     // crawler config
     const crawler = new Apify.PuppeteerCrawler({
+        launchContext: {
+            args: ['--incognito'],
+        },
         requestList,
         requestQueue,
         useSessionPool: true,
